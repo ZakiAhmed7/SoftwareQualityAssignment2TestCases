c0e26400b3f9a0a94a367e8a348a6a1dc4534c39
ZakiAhmed7 <syedzakiahmed711@gmail.com>
2024-03-06 00:51:33 -0500

    Equaleteral and Isosceles Triangle

--- TestCase/Class1.cs
+++ TestCase/Class1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
@@ -9,7 +10,7 @@ using TriangleSolver;
 namespace TestCase
 {
     public class Class1 {
-
+        // Test case for Equal lateral triangle
         [Test]
         public void equalLaterailTriangle()
         {
@@ -25,8 +26,51 @@ namespace TestCase
             string actual = Triangle.AnalyzeTriangle(angle1, angle2, angle3);
             Assert.That(expected, Is.EqualTo(actual));
         }
+        // Test case for Isosceles triangle
+        [TestCase(5, 5, 7)]
+        [TestCase(7, 5, 5)]
+        [TestCase(5, 7, 5)]
+        public void validIsoscelesTriangle(int x, int y, int z) {
 
-        
+            string actual = Triangle.AnalyzeTriangle(x, y, z);
+
+            Assert.That("Isosceles triangle", Is.EqualTo(actual));
+        }
+        // Test case for valid triangle
+        [TestCase(0, 5, 7)]
+        [TestCase(7, 0, 5)]
+        [TestCase(5, 7, 0)]
+        public void validTriangle(int x, int y, int z)
+        {
+
+            string actual = Triangle.AnalyzeTriangle(x, y, z);
+
+            Assert.That("Invalid Triangle - a zero has been detected", Is.EqualTo(actual));
+        }
+
+        [TestCase(-1, 5, 7)]
+        [TestCase(7, -1, 5)]
+        [TestCase(5, 7, -1)]
+        public void checkForInvalidTriangle(int x, int y, int z)
+        {
+
+            string actual = Triangle.AnalyzeTriangle(x, y, z);
+
+            Assert.That("INVALID!!", Is.EqualTo(actual));
+        }
+
+        [TestCase(3, 4, 5)]
+        [TestCase(10, 9, 8)]
+        [TestCase(8, 15, 17)]
+        [TestCase(7, 10, 12)]
+        [TestCase(8, 11, 13)]
+        public void validScaleneTriangle(int x, int y, int z)
+        {
+
+            string actual = Triangle.AnalyzeTriangle(x, y, z);
+
+            Assert.That("Scalene triangle", Is.EqualTo(actual));
+        }
     }
 
 
